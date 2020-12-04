8e52edc4d9c3806de66c8c25bafe91061cf50750
christopheraguilera <christopher.aguilera.mad@gmail.com>
2020-12-04 17:58:53 -0300

    v.2

--- test.js
+++ test.js
@@ -2,31 +2,38 @@ const axios = require("axios");
 const { v4: uuidv4 } = require('uuid')
 const moment = require('moment')
 const _ = require('lodash')
-const chalk = require('chalk')
-let usuarios = []
-
-
-for (let i = 1; i < 8; i++){
+const chalk = require('chalk');
+let j = 1
+let guardar =[]
+let row = ''
 axios
-.get("https://randomuser.me/api/")
+.get("https://randomuser.me/api/?results=10")
 .then((data) => {
 const info = data.data.results;
-const nombre = info[0].name.first
-const apellido = info[0].name.last
-const consulta = {
-    fecha: moment().add(10000, 'days').format('MMM Do YY'),
-    ID: uuidv4(),
-    }
-const usuario = `${i}. Nombre: ${nombre} - Apellido: ${apellido} - ID: ${consulta.ID} - Timestamp: ${consulta.fecha}`
-usuarios.push(usuario)
-if(i == 7){
-console.log('resultado:')
-console.log(chalk.blue.bgWhite.bold(_.partition(usuarios, (n) => n)))
-}
+    _.partition(info, (n) => {
+    usuarios(n)
+    })
 })
 .catch((e) => {
 console.log(e);
 });
-}
 
-// /nodemon index.js
+function usuarios(n){
+    const nombre = n.name.first
+    const apellido = n.name.last
+    const consulta = {
+        fecha: moment().format('MMMM Do YYYY'),
+        hora: moment().format('hh:mm:ss'),
+        ID: uuidv4().slice(0, 6),
+        }
+    const usuario = `${j}. Nombre: ${nombre} - Apellido: ${apellido} - ID: ${consulta.ID} - Timestamp: ${consulta.fecha}, ${consulta.hora}`
+    guardar.push(usuario)
+    j = j+1
+    console.log(chalk.blue.bgWhite.bold(usuario))
+    if (j == 11){
+    _.partition(guardar, (n) => {
+        row = row + `<li>${n}</li>`
+        })
+        console.log(row)
+    }
+}
